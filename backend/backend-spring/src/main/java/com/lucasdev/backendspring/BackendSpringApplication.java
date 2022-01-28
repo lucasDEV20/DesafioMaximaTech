72f907c6267da58ffdae0a13f7a8061d1e1a5925
lucasDEV20 <lucasgoias11@gmail.com>
2022-01-27 23:27:31 -0300

    subindo backend inicial

--- backend/backend-spring/src/main/java/com/lucasdev/backendspring/BackendSpringApplication.java
+++ backend/backend-spring/src/main/java/com/lucasdev/backendspring/BackendSpringApplication.java
@@ -1,13 +1,13 @@
-package br.com.backend.backend;
+package com.lucasdev.backendspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(BackendSpringApplication.class, args);
 	}
 
 }
--- backend/backend-spring/src/test/java/com/lucasdev/backendspring/BackendSpringApplicationTests.java
+++ backend/backend-spring/src/test/java/com/lucasdev/backendspring/BackendSpringApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.backend.backend;
+package com.lucasdev.backendspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BackendSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
